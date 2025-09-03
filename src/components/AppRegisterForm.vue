a0185ab98710e798e5d54764f5b10def394b870c
Nadja <nadja.luckhaupt@gmail.com>
2025-09-03 12:14:26 +0200

    Initial commit

---
+++ src/components/AppRegisterForm.vue
@@ -0,0 +1,182 @@
+<template>
+  <div
+    class="text-white text-center font-bold p-4 rounded mb-4"
+    v-if="reg_show_alert"
+    :class="reg_alert_variant"
+  >
+    {{ reg_alert_msg }}
+  </div>
+  <VeeForm :validation-schema="schema" @submit="register" :initial-values="userData">
+    <!-- Name -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Name</label>
+      <VeeField
+        type="text"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Name"
+        name="name"
+      />
+      <VeeErrorMessage class="text-red-600" name="name" />
+    </div>
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <VeeField
+        type="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+        name="email"
+      />
+      <VeeErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Age -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Age</label>
+      <VeeField
+        type="number"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        name="age"
+      />
+      <VeeErrorMessage class="text-red-600" name="age" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <VeeField name="password" :bails="false" v-slot="{ field, errors }"
+        ><input
+          placeholder="Password"
+          type="password"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          v-bind="field"
+        />
+        <div class="text-red-600" v-for="error in errors" :key="error">
+          {{ error }}
+        </div></VeeField
+      >
+    </div>
+    <!-- Confirm Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Confirm Password</label>
+      <VeeField
+        type="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Confirm Password"
+        name="confirm_password"
+      />
+      <VeeErrorMessage class="text-red-600" name="confirm_password" />
+    </div>
+    <!-- Country -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Country</label>
+      <VeeField
+        as="select"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        name="country"
+      >
+        <option value="USA">USA</option>
+        <option value="Mexico">Mexico</option>
+        <option value="Germany">Germany</option>
+        <option value="Antarctica">Antarctica</option>
+      </VeeField>
+      <VeeErrorMessage class="text-red-600" name="country" />
+    </div>
+    <!-- TOS -->
+    <div class="mb-3 pl-6">
+      <VeeField
+        type="checkbox"
+        class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+        name="tos"
+        value="1"
+      />
+      <label class="inline-block">Accept terms of service</label>
+      <VeeErrorMessage class="text-red-600 block" name="tos" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      :disabled="reg_in_submission"
+    >
+      Submit
+    </button>
+  </VeeForm>
+</template>
+
+<script>
+import { auth, usersCollection } from '@/includes/firebase'
+import { mapWritableState } from 'pinia'
+import useUserStore from '@/stores/user'
+
+export default {
+  name: 'AppRegisterForm',
+  data() {
+    return {
+      schema: {
+        name: 'required|min:3|max:100|alpha_spaces',
+        email: 'required|min:3|max:100|email',
+        age: 'required|min_value:18|max_value:200',
+        password: 'required|min:9|max:100|excluded:password',
+        confirm_password: 'confirmed:@password',
+        country: 'required|country_excluded:Antarctica',
+        tos: 'tos',
+      },
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:9|max:100',
+      },
+      userData: {
+        country: 'Germany',
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created.',
+    }
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn']),
+  },
+  methods: {
+    async register(values) {
+      this.reg_show_alert = true
+      this.reg_in_submission = true
+      this.reg_alert_variant = 'bg-blue-500'
+      this.reg_alert_msg = 'Please wait! Your account is being created.'
+
+      let userCred = null
+
+      try {
+        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later.'
+        console.error('auth: ', error)
+
+        return
+      }
+
+      try {
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        })
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later.'
+        console.error('collection: ', error)
+
+        return
+      }
+
+      this.userLoggedIn = true
+
+      this.reg_alert_variant = 'bg-green-500'
+      this.reg_alert_msg = 'Success! Your account has been created.'
+      console.log(userCred)
+    },
+  },
+}
+</script>
