a4033d7227b008c97d7ea693ffc7fce0c0368bd1
Nadja <nadja.luckhaupt@gmail.com>
2025-09-03 14:44:47 +0200

    routes

---
+++ src/router/constants.js
@@ -0,0 +1,13 @@
+import AboutView from '@/views/AboutView.vue'
+import HomeView from '@/views/HomeView.vue'
+
+export const routes = [
+  {
+    path: '/',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    component: AboutView,
+  },
+]
