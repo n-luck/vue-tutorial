41790220af78b8e3a1d3e871ac33d18177655724
Nadja <nadja.luckhaupt@gmail.com>
2025-09-04 13:19:05 +0200

    links & redirect

--- src/router/constants.js
+++ src/router/constants.js
@@ -1,13 +1,29 @@
 import AboutView from '@/views/AboutView.vue'
 import HomeView from '@/views/HomeView.vue'
+import ManageView from '@/views/ManageView.vue'
 
 export const routes = [
   {
+    name: 'home',
     path: '/',
     component: HomeView,
   },
   {
+    name: 'about',
     path: '/about',
     component: AboutView,
+  },
+  {
+    name: 'manage',
+    path: '/manage-music',
+    component: ManageView,
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ]
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import { routes } from './constants'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes,
+  routes,
+  linkExactActiveClass: "text-yellow-500"
 })
 
 export default router
